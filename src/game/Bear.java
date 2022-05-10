432f79abd5863e1863b22634004c5a8c6bd24561
YusufAtesmen <yusufatesmen@gmail.com>
2022-05-10 10:29:09 +0300

    Game 10/05/2022

--- src/game/Bear.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-public class Bear extends Obstacle {
-    public Bear(){
-        super("Ayı",7,20,12,2);
-    }
-}
--- src/gameYusuf/Zombie.java
+++
@@ -1,9 +0,0 @@
-package gameYusuf;
-
-import game.Obstacle;
-
-public class Zombie extends Obstacle {
-    public Zombie(){
-        super("Zombi",3,10,4,3);
-    }
-}
